f22feef6918512fd775b3e0ae1478e10ea80f7af
pratv3:~# <74216667+pratv3@users.noreply.github.com>
2021-09-09 20:30:32 +0530

    update.sh
    This may better

--- TBomb.sh
+++ TBomb.sh
@@ -24,7 +24,7 @@ do
 rm *.xxx >/dev/null 2>&1
 clear
 echo -e "\e[1;31m"
-figlet TBomb
+figlet Fuck
 echo -e "\e[1;34m Created By \e[1;32m"
 toilet -f mono12 -F border Raju and Rathore
 echo -e "\e[4;34m This Bomber Was Created By Mohit \e[0m"
